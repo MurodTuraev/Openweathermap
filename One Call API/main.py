649c30c963ae617752e97d8c518764a024ca02ec
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-05 11:33:07 +0500

    One call

---
+++ One Call API/main.py
@@ -0,0 +1,13 @@
+import requests
+import json
+from pprint import pprint
+url = 'https://api.openweathermap.org/data/2.5/onecall'
+parametr = {
+    'lat': '33.441792',
+    'lon': '-94.037689',
+    'exclude': 'hourly,daily',
+    'appid': '493a14bdcf21b497b383ede86e1101ba'
+}
+
+data = requests.get(url, params=parametr).json()
+pprint(data)
