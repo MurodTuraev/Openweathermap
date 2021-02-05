e0fd6cbd3ba92bea964889c81c87c09b8638ac41
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-02-05 09:26:41 +0500

    with params

--- Current weather data/main.py
+++ Current weather data/main.py
@@ -1,8 +1,10 @@
 import requests
 import json
 from pprint import pprint
-api_key = '26e020a7d39d87e718b910e1e37baf15'
-city_name = 'Samarkand'
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
-data = requests.get(url).json()
+url = f'http://api.openweathermap.org/data/2.5/weather'
+parametr = {
+    'q': 'Samarkand',
+    'appid': '26e020a7d39d87e718b910e1e37baf15'
+}
+data = requests.get(url, params=parametr).json()
 pprint(data)
